243be52d582f27245954ca0c169833c541925266
Anandkumar Patel <anand@runnable.com>
2016-09-29 14:47:45 -0700

    add auto-migrator

--- migrations/20160928153423_create_events_table.js
+++ migrations/20160928153423_create_events_table.js
@@ -1,22 +1,12 @@
 'use strict'
 const log = require('logger').child({ migration: 'create_events_table' })
+const metricEvent = require('models/data-structures/metric-event')
+const MigrationUtils = require('./migration-utils')
 
 exports.up = (knex, Promise) => {
   const createTable = knex.schema.createTable('events', (table) => {
     table.increments('id').primary()
-    table.string('event_name').notNullable()
-    table.string('time_published').notNullable()
-    table.string('time_recevied').notNullable()
-    table.string('transaction_id').notNullable()
-    // optional
-    table.string('publisher_app_name').nullable()
-    table.string('previous_event_name').nullable()
-    // tags
-    table.string('org').nullable()
-    table.string('stack').nullable()
-    table.string('host').nullable()
-    table.string('template').nullable()
-    table.string('branch').nullable()
+    MigrationUtils.configureTableFromJoi(table, metricEvent._METRIC_EVENT_SCHEMA)
   })
   log.debug({ createTable }, 'migration up')
   return createTable
