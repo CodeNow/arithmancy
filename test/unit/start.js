e5033dca5ae84e6a1d6106bf70cd2b632a9ca8fc
Anandkumar Patel <anand@runnable.com>
2016-09-30 10:08:34 -0700

    initialize postgres connection

--- test/unit/start.js
+++ test/unit/start.js
@@ -5,6 +5,7 @@ const Promise = require('bluebird')
 const sinon = require('sinon')
 
 const datadogForwarder = require('models/forwarders/datadog-forwarder')
+const postgresStore = require('models/persistent-stores/postgres-store')
 const publisher = require('external/publisher')
 const start = require('start')
 const workerServer = require('external/worker-server')
@@ -22,6 +23,7 @@ describe('start.js unit test', () => {
     beforeEach((done) => {
       sinon.stub(publisher, 'start')
       sinon.stub(datadogForwarder, 'initialize')
+      sinon.stub(postgresStore, 'initialize')
       sinon.stub(workerServer, 'start')
       sinon.stub(ErrorCat, 'report')
       sinon.stub(process, 'exit')
@@ -30,6 +32,7 @@ describe('start.js unit test', () => {
 
     afterEach((done) => {
       datadogForwarder.initialize.restore()
+      postgresStore.initialize.restore()
       publisher.start.restore()
       workerServer.start.restore()
       ErrorCat.report.restore()
@@ -41,11 +44,13 @@ describe('start.js unit test', () => {
       publisher.start.resolves()
       workerServer.start.resolves()
       datadogForwarder.initialize.resolves()
+      postgresStore.initialize.resolves()
 
       start().asCallback((err) => {
         if (err) { return done(err) }
         sinon.assert.callOrder(
           datadogForwarder.initialize,
+          postgresStore.initialize,
           publisher.start,
           workerServer.start
         )
